293d7922df773129ff78788fb95e812fc26c4342
SlackPkgMaster <charles.ross@dawsoncollege.qc.ca>
2025-09-24 11:57:54 -0400

    Form processing using PHP

---
+++ formprocess.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>Results</head>
+
+    <body>
+        <h1>Result of GET request</h1>
+        <p><?= var_dump($_GET) ?></p>
+
+        <h1>Result of POST request</h1>
+        <p><?= var_dump($_POST) ?></p>
+    </body>
+</html>
