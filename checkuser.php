c9815d7ee73301565c67d5f7ba2f6189a2bac4e2
SlackPkgMaster <charles.ross@dawsoncollege.qc.ca>
2025-09-24 18:38:00 -0400

    PHP check user test

--- checkuser.php
+++ checkuser.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 
 <html>
-    <head></head>
+    <head>
+        <title>Results</title> 
+    </head>
     <body>
-        <p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
+        <h2>Shirts Requested </h2>
+        <?php if (isset($_POST['shirts'])) { ?>
+            <ul>
+            <?php foreach ($x as $_POST['shirts']) : ?>
+            <li><?= htmlspecialchars($x) ?>  </li>
+            </ul>
+            <?php endforeach 
+        } ?>
     </body>
 </html>
