2d8c878cfb53dbc4d9865ac141517971a32ff0dc
SlackPkgMaster <charles.ross@dawsoncollege.qc.ca>
2025-09-24 16:10:34 -0400

    PHP form processing

--- formprocess.php
+++ formprocess.php
@@ -2,12 +2,26 @@
 
 <html>
     <head>Results</head>
-
+    <?php 
+        if isset($_POST)
+    ?>
     <body>
-        <h1>Result of GET request</h1>
-        <p><?= var_dump($_GET) ?></p>
-
-        <h1>Result of POST request</h1>
-        <p><?= var_dump($_POST) ?></p>
+        <h1>Result of Form request</h1>
+        <ul>
+            <li>Demi-God Slut shirt : <?=  ?></li>
+            <li>Chasm of Filth shirt : <?=  ?></li>
+            <li>Maggot Desecration shirt : <?=  ?></li>
+            <li>Graves of Piss shirt : <?=  ?></li>
+            <li>Ninja Sex Party shirt : <?=  ?></li>
+        </ul>
+        <p>Shirt size : <?= $_POST['sizes'] ?></p>
+        <p>Shirt color : <?= $_POST['color'] ?>
+        <ul>
+            <li>Demi-God Slut shirt : <?= $_POST['dgs_shirt'] ?></li>
+            <li>Chasm of Filth shirt : <?= $_POST['cof_shirt'] ?></li>
+            <li>Maggot Desecration shirt : <?= $_POST['md_shirt'] ?></li>
+            <li>Graves of Piss shirt : <?= $_POST['gop_shirt'] ?></li>
+            <li>Ninja Sex Party shirt : <?= $_POST['nsp_shirt'] ?></li>
+        </ul>
     </body>
 </html>
